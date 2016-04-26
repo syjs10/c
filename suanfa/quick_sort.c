bfb64a58f107571ecc966aee0eea1e30d19e5c79
syjs10 <350172898@qq.com>
2016-04-26 21:50:35 +0800

    c

--- suanfa/quick_sort.c
+++ suanfa/quick_sort.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 void sort(int *a, int l, int r){
-    if (l < r) {
+    if (l > r) {
         return;
-    } else {
+    }
         int i = l;
         int j = r;
         int key = a[l];
@@ -11,7 +11,7 @@ void sort(int *a, int l, int r){
             while (i < j && key <= a[j]) {
                 j--;
             }
-            a[j] = a[i];
+            a[i] = a[j];
             while (i < j && key >= a[i]) {
                 i++;
             }
@@ -20,13 +20,21 @@ void sort(int *a, int l, int r){
         a[i] = key;
         sort(a, l, i - 1);
         sort(a, i + 1, r);
-    }
+
 }
 int main(int argc, char const *argv[]) {
     int n;
     while (~scanf("%d", &n)){
         int *p = (int *)malloc(sizeof(int) * n);
+        for (int i = 0; i < n; i++){
+            scanf("%d", &p[i]);
+        }
         sort(p, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            printf("%d ", p[i]);
+        }
+        printf("\n");
+        free(p);
     }
     return 0;
 }
